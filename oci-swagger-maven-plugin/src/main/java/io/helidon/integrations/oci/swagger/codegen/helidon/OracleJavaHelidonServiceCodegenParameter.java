fe942d154795a857be700928b38a2a379920ef4b
Arjav Desai <arjav.desai@oracle.com>
2023-10-31 11:50:01 -0500

    helidon oci service generator

---
+++ oci-swagger-maven-plugin/src/main/java/io/helidon/integrations/oci/swagger/codegen/helidon/OracleJavaHelidonServiceCodegenParameter.java
@@ -0,0 +1,24 @@
+package io.helidon.integrations.oci.swagger.codegen.helidon;
+
+import com.oracle.bmc.sdk.swagger.codegen.OracleCodegenConfig;
+import com.oracle.bmc.sdk.swagger.codegen.model.java.OracleJavaCodegenParameter;
+import io.swagger.codegen.CodegenParameter;
+import io.swagger.models.Swagger;
+import lombok.NonNull;
+
+public class OracleJavaHelidonServiceCodegenParameter extends OracleJavaCodegenParameter {
+    public OracleJavaHelidonServiceCodegenParameter(
+            CodegenParameter original,
+            @NonNull OracleCodegenConfig codegen,
+            @NonNull Swagger spec) {
+        super(original, codegen, spec);
+    }
+
+    @Override
+    public CodegenParameter copy() {
+        CodegenParameter copy = super.copy();
+        // no idea why this is not being copied over in CodegenParameter::copy (super.copy())
+        copy.isPrimitiveType = this.isPrimitiveType;
+        return copy;
+    }
+}
