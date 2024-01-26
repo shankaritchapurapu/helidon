f1a95318df8b915efcd82cecdf57554903d5ac03
Keith Lustria <keith.lustria@oracle.com>
2024-01-26 21:58:13 +0000

    Pull request #32: Add T2 metric testing using SSH tunneling
    Merge in HLDN/oci-helidon from add_t2_metric_test to helidon-2.x
    
    Squashed commit of the following:
    
    commit d68659cc9ea0e846ff53b8d6ae009fe6d6f1d615
    Author: Keith Lustria <keith.lustria@oracle.com>
    Date:   Fri Jan 26 12:44:24 2024 -0800
    
        Add T2 metric testing using SSH tunneling
    
        Test includes the following:
        1. T2 api test using metric-lib and metrics-overlay
        2. OCI Monitoring SDK test

--- integration-tests/README.md
+++ integration-tests/README.md
@@ -14,3 +14,6 @@ Create a submodule named `<module>-test` for each module under test.
 ```shell
 mvn clean package
 ```
+
+## Creating a test module that uses SSH Tunneling
+Using SSH Tunnel as an approach for performing OCI native integration tests helps promote fast iteration to code changes. Check out [oci-t2-metrics-test readme](oci-t2-metrics-test/README.md) for more details on how to use [oci-t2-metrics-test](oci-t2-metrics-test) module as an example for creating a test that uses SSH Tunneling.
---
+++ integration-tests/oci-t2-metrics-test/src/test/java/com/oracle/test/t2metrics/TelemetrySTTest.java
@@ -0,0 +1,174 @@
+package com.oracle.test.t2metics;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import com.oracle.bmc.auth.BasicAuthenticationDetailsProvider;
+import com.oracle.bmc.ClientConfiguration;
+import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
+import com.oracle.bmc.auth.InstancePrincipalsAuthenticationDetailsProvider;
+import com.oracle.bmc.monitoring.MonitoringClient;
+import com.oracle.bmc.monitoring.model.Datapoint;
+import com.oracle.bmc.monitoring.model.MetricDataDetails;
+import com.oracle.bmc.monitoring.model.PostMetricDataDetails;
+import com.oracle.bmc.monitoring.requests.PostMetricDataRequest;
+import com.oracle.bmc.retrier.RetryConfiguration;
+
+import com.oracle.pic.telemetry.commons.metrics.Metrics;
+import com.oracle.pic.telemetry.commons.metrics.TelemetryReporter;
+import com.oracle.pic.telemetry.commons.metrics.TelemetryReporterBuilder;
+import com.oracle.pic.telemetry.overlay.clients.InstanceMetadata;
+import com.oracle.pic.telemetry.overlay.clients.OverlayInstanceMetadataClient;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+class TelemetrySTTest {
+    private static final String PROJECT = "xxxx";
+    private static final String FLEET_T2 = "t2-api";
+    private static final String FLEET_MONITORING = "monitoring-sdk";
+    private static final String METRIC_NAME = "t2metric";
+    private static final String INSTANCE_METADATA_BASE_URL = "http://localhost:8000/opc/v2/";
+    private static final String INSTANCE_METADATA_INSTANCE_URL = INSTANCE_METADATA_BASE_URL + "instance/";
+    private static MonitoringClient monitoringClient;
+    private static String requestId;
+    public enum InstanceMetadataProperty {
+        /*
+         * The name should match metadata property name here https://docs.cloud.oracle.com/iaas/Content/Compute/Tasks/gettingmetadata.htm
+         */
+        DISPLAY_NAME("displayName"),
+        COMPARTMENT_ID("compartmentId"),
+        CANONICAL_REGION_NAME("canonicalRegionName"),
+        OCI_AD_NAME("ociAdName"),
+        FAULT_DOMAIN("faultDomain"),
+        HOST_NAME("hostname");
+        String name;
+        InstanceMetadataProperty(String name) {
+            this.name = name;
+        }
+    }
+    private static final Logger LOGGER = Logger.getLogger(TelemetrySTTest.class.getName());
+
+    @BeforeAll
+    static void beforeAll() {
+        LOGGER.info("Instantiating Instance Principal");
+        InstancePrincipalsAuthenticationDetailsProvider instancePrincipalAuth =
+                InstancePrincipalsAuthenticationDetailsProvider.builder()
+                        .metadataBaseUrl(INSTANCE_METADATA_BASE_URL)
+                        .detectEndpointRetries(1)
+                        .timeoutForEachRetry(3000)
+                        .build();
+        LOGGER.info("Instantiating Monitoring Client");
+        monitoringClient = MonitoringClient.builder()
+                .configuration(ClientConfiguration.builder()
+                                       .connectionTimeoutMillis(3000)
+                                       .readTimeoutMillis(3000)
+                                       .retryConfiguration(RetryConfiguration.SDK_DEFAULT_RETRY_CONFIGURATION)
+                                       .build())
+                .build(instancePrincipalAuth);
+        String endpoint = monitoringClient.getEndpoint().replaceFirst("telemetry\\.", "telemetry-ingestion.");
+        LOGGER.info("Setting monitoring endpoint to '" + endpoint + "'");
+        monitoringClient.setEndpoint(endpoint);
+    }
+
+    @Test
+    void T2APITest() {
+        LOGGER.info("Instantiating TelemetryReporter");
+        TelemetryReporter reporter = new TelemetryReporterBuilder()
+                .monitoringClient(monitoringClient)
+                .project(PROJECT)
+                .fleet(FLEET_T2)
+                // .instanceMetadataEndpointOverride(INSTANCE_METADATA_INSTANCE_URL)
+                // .useMetadataService(true)
+                .region(getFieldValueByName(InstanceMetadataProperty.CANONICAL_REGION_NAME))
+                .hostname(getFieldValueByName(InstanceMetadataProperty.DISPLAY_NAME))
+                .availabilityDomain(getFieldValueByName(InstanceMetadataProperty.OCI_AD_NAME))
+                .faultDomain(getFieldValueByName(InstanceMetadataProperty.FAULT_DOMAIN))
+                .build();
+
+        LOGGER.info("Initializing T2 Metrics");
+        Metrics.init(reporter);
+
+        LOGGER.info("Sending metrics");
+        Metrics.emit(METRIC_NAME, 5);
+
+        LOGGER.info("Sending metrics");
+        LOGGER.info("Shutting down T2 Metrics");
+        Metrics.shutdown();
+    }
+
+    @Test
+    void OCIMonitoringSDKTest() {
+        LOGGER.info("Creating datapoints and dimensions");
+        List<Datapoint> datapoints = List.of(Datapoint.builder()
+                                                     .timestamp(Date.from(Instant.now()))
+                                                     .value(10.0)
+                                                     .count(1)
+                                                     .build());
+        Map<String, String> dimensions = Map.of("name1", "value1",
+                                                "name2", "value2",
+                                                "name3", "value3");
+
+        LOGGER.info("Assembing metricDetails");
+        MetricDataDetails metricDataDetails =
+                MetricDataDetails.builder()
+                        // T2 metric compartment for OC1
+                        .compartmentId("ocid1.compartment.oc1..aaaaaaaagixeyxsjv643gwx5vf6dkuwmvvf4dlf7k6sobwzbjrtce4lvndwq")
+                        .namespace(PROJECT)                      // equivalent to T2 project
+                        .resourceGroup(FLEET_MONITORING)         // equivalent to T2 fleet
+                        .name(METRIC_NAME)
+                        .datapoints(datapoints)
+                        .dimensions(dimensions)
+                        .build();
+
+        PostMetricDataDetails postMetricDataDetails =
+                PostMetricDataDetails.builder().metricData(List.of(metricDataDetails)).build();
+
+        PostMetricDataRequest postMetricDataRequest =
+                PostMetricDataRequest.builder()
+                        .postMetricDataDetails(postMetricDataDetails)
+                        .build();
+
+        LOGGER.info("Posting metrics");
+        monitoringClient.postMetricData(postMetricDataRequest);
+        LOGGER.info("Metrics successfully sent");
+    }
+
+    // Used to extract a field from Instance Metadata Service
+    private static String getFieldValueByName(InstanceMetadataProperty field) {
+        String uri = String.format(INSTANCE_METADATA_BASE_URL + "instance/%s", field.name);
+        try {
+            Client client = ClientBuilder.newBuilder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(12, TimeUnit.SECONDS)
+                    .build();
+            Response response = client.target(uri).request().header("Authorization", "Bearer Oracle").get();
+            String requestId = response.readEntity(String.class);
+            return requestId;
+        } catch (Exception exc) {
+            System.out.print("Failed to get " + field.name + " from Instance Metadata Service: " + exc);
+        }
+        return null;
+    }
+
+    // private InstanceMetadata getInstanceMetadata() {
+    //     OverlayInstanceMetadataClient metadataClient =
+    //             new OverlayInstanceMetadataClient(INSTANCE_METADATA_INSTANCE_URL);
+    //     return metadataClient.getInstanceMetadata();
+    // }
+}
