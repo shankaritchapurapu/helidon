ce72844a5126a299369dfafbd9609838b54b20e0
Joe DiPol <joe.dipol@oracle.com>
2024-09-09 20:19:52 +0000

    Pull request #84: OWASP dependency check
    Merge in HLDN/oci-helidon from ~JDIPOL/oci-helidon:owasp-dep-check to main
    
    Squashed commit of the following:
    
    commit 2fbe38861e57ebf09ac509c96ecb395fb78d506a
    Author: Joe Di Pol <joe.dipol@oracle.com>
    Date:   Mon Sep 9 10:47:47 2024 -0700
    
        Fix copyright
    
    commit a476ea9a3c75e7624712bfd20739b2694fa52e3a
    Author: Joe Di Pol <joe.dipol@oracle.com>
    Date:   Mon Sep 9 10:34:10 2024 -0700
    
        Remove owasp dep check excludes
    
    commit 7b1d279a6df033667ffdbb205ddd1473417b529a
    Author: Joe Di Pol <joe.dipol@oracle.com>
    Date:   Mon Sep 9 10:32:29 2024 -0700
    
        Add owasp dependency check plugin

---
+++ etc/scripts/owasp-dependency-check.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2024 Oracle and/or its affiliates.
+#
+
+set -o pipefail || true  # trace ERR through pipes
+set -o errtrace || true # trace ERR through commands and functions
+set -o errexit || true  # exit the script if any statement returns a non-true return value
+
+# Path to this script
+if [ -h "${0}" ] ; then
+    SCRIPT_PATH="$(readlink "${0}")"
+else
+    # shellcheck disable=SC155
+    SCRIPT_PATH="${0}"
+fi
+readonly SCRIPT_PATH
+
+# Path to the root of the workspace
+# shellcheck disable=SC2046
+WS_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; cd ../.. ; pwd -P)
+
+on_error(){
+    CODE="${?}" && \
+    set +x && \
+    printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
+        "${CODE}" "${BASH_SOURCE[0]}" "${LINENO}" "${BASH_COMMAND}"
+}
+trap on_error ERR
+
+RESULT_FILE=$(mktemp -t XXXdependency-check-result)
+readonly  RESULT_FILE
+
+die() { cat "${RESULT_FILE}" ; echo "Dependency report in ${WS_DIR}/target" ; echo "${1}" ; exit 1 ;}
+
+if [ "${PIPELINE}" = "true" ] ; then
+    # If in pipeline do a priming build before scan
+    # shellcheck disable=SC2086
+    mvn ${MAVEN_ARGS} -f "${WS_DIR}"/pom.xml clean install -DskipTests
+fi
+
+# Setting NVD_API_KEY is not required but improves behavior of NVD API throttling
+
+# shellcheck disable=SC2086
+mvn ${MAVEN_ARGS} -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN org.owasp:dependency-check-maven:aggregate \
+        -f "${WS_DIR}"/pom.xml \
+        -Dtop.parent.basedir="${WS_DIR}" \
+        -Dnvd-api-key="${NVD_API_KEY}" \
+        > "${RESULT_FILE}" || die "Error running the Maven command"
+
+grep -i "One or more dependencies were identified with known vulnerabilities" "${RESULT_FILE}" \
+    && die "CVE SCAN ERROR" || echo "CVE SCAN OK"
